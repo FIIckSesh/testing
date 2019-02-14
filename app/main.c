efa73bef454f0662cbc7078ffb74ac76f9f947a1
egorhristoforov <hristofo@cs.karelia.ru>
2019-02-14 20:29:15 +0300

    Init2

--- app/main.c
+++ app/main.c
@@ -6,9 +6,10 @@
 int main()
 {
     values val;
+    int check;
     double a, b, c;
     printf("Введите коэффициенты: \n");
-    scanf("%lf %lf %lf", &a, &b, &c);
+    check = scanf("%lf %lf %lf", &a, &b, &c);
     val = equatation(a, b, c);
     if (val.flag == 2) printf("Solved: x1 = %.2lf, x2 = %.2lf \n", val.x1, val.x2);
     return 0;
